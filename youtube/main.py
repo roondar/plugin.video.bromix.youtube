57486ffc61c1c2d1c402bb0fb3a4daa50dc0d407
bromix <bromix@gmx.net>
2014-08-01 20:18:45 +0200

    added test for some low resolutions

--- youtube/main.py
+++ youtube/main.py
@@ -4,8 +4,11 @@ import time
 import video
 
 if __name__ == "__main__":
-    # normal video - no age-restriction, no signature
+    """normal video - no age-restriction, no signature"""
     #videoInfos = video.getVideoStreamInfos('IKiaiHFtQCY')
+    
+    """test 480p and 360p"""
+    #videoInfos = video.getVideoStreamInfos('p4zdj-HO0uc')
     
     #videoInfos = getVideoStreamInfos('zJ_ld0PjdQg')
     #videoInfos = getVideoStreamInfos('yZ2RCJ28UPQ')
@@ -13,14 +16,15 @@ if __name__ == "__main__":
     #videoInfos = video.getVideoStreamInfos('kqYuEKtDvAc')
     
     start = time.time()
-    videoInfos = video.getVideoStreamInfos('IKiaiHFtQCY')
+    videoInfos = video.getVideoStreamInfos('p4zdj-HO0uc')
     end = time.time()
     
     diff = end-start
     
     stream1080 = video.getBestFittingVideoStreamInfo(videoInfos=videoInfos, size=1080)
-    stream480 = video.getBestFittingVideoStreamInfo(videoInfos=videoInfos, size=480)
     stream720 = video.getBestFittingVideoStreamInfo(videoInfos=videoInfos, size=720)
+    stream480 = video.getBestFittingVideoStreamInfo(videoInfos=videoInfos, size=480)
+    stream360 = video.getBestFittingVideoStreamInfo(videoInfos=videoInfos, size=360)
     
     url = stream720.getUrl()
     pass
