14feab586cb614f6e3aa8e68c9494576a958aa6b
bromix <bromix@gmx.net>
2014-12-02 18:43:22 +0100

    FIX: remove next page for related videos (because APIv3 is broken)

--- resources/lib/youtube/provider.py
+++ resources/lib/youtube/provider.py
@@ -275,6 +275,12 @@ class Provider(kodion.AbstractProvider):
         video_id = context.get_param('video_id', '')
         if video_id:
             json_data = self.get_client(context).get_related_videos(video_id=video_id, page_token=page_token)
+
+            # we remove the token for a next page, because the APIv3 is broken at this point
+            if 'nextPageToken' in json_data:
+                del json_data['nextPageToken']
+                pass
+
             result.extend(v3.response_to_items(self, context, json_data))
             pass
 
